35fe329c6be704def9d7ac228208548091053494
Gabriel <usr4035@salesianos-lacuesta.net>
2025-04-29 10:21:00 +0100

    feat(App.java): Display menu options

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Menu:");
+        System.out.println("1. añadrir persona");
+        System.out.println("2. elimnar persona");
+        System.out.println("3. salir personas");
     }
 }
